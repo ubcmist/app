85f21b8652dde22ffd219f59798f98a0a73381a9
Farid Faraji <faridfaraji@FAMP.hitronhub.home>
2019-01-11 22:49:01 -0800

    location

--- components/location.js
+++ components/location.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Platform, Text, View, StyleSheet } from 'react-native';
-import { Constants, Location, Permissions } from 'expo';
+import { Constants, Location, Permissions,TaskManager } from 'expo';
+export const BACKGROUND_LOCATION_UPDATES_TASK = 'background-location-updates'
+TaskManager.defineTask(BACKGROUND_LOCATION_UPDATES_TASK, handleLocationUpdate)
+
 
 class ExpoLocation extends Component {
   state = {
@@ -11,7 +14,7 @@ class ExpoLocation extends Component {
   componentWillMount() {
     if (Platform.OS === 'android' && !Constants.isDevice) {
       this.setState({
-        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+        errorMessage: 'Does not work!',
       });
     } else {
       this._getLocationAsync();
@@ -34,6 +37,7 @@ class ExpoLocation extends Component {
     } else if (this.state.location) {
       text = JSON.stringify(this.state.location);
     }
+    console.log(text);
 
 
   };
@@ -42,6 +46,34 @@ class ExpoLocation extends Component {
     
     return null;
   }
+}
+
+
+
+export async function handleLocationUpdate({ data, error }) {
+    console.log('location update')
+    const loc = "hubby";
+    if (error) {
+		return
+	}
+     if (data) {
+        try {
+            
+            //console.log('locations',location)
+        } catch (error) {
+            console.log('the error',error)
+        }
+    }
+}
+
+export async function initializeBackgroundLocation(){
+    let isRegistered = await TaskManager.isTaskRegisteredAsync(BACKGROUND_LOCATION_UPDATES_TASK)
+    if (!isRegistered) await Location.startLocationUpdatesAsync(BACKGROUND_LOCATION_UPDATES_TASK, {
+        accuracy: Location.Accuracy.High,
+        /* after edit */
+        timeInterval: 2500,
+        distanceInterval: 5,
+    })
 }
 
 
